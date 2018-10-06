5297d7c253e39949e48649568fa4c1596c8181c3
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-10-06 17:33:11 +0200

    adding code

---
+++ Code/Game/maps/Mazmorra.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mazmorra" tilewidth="16" tileheight="16" margin="1" tilecount="36" columns="6">
+ <image source="Mazmorra.png" width="99" height="99"/>
+</tileset>
---
+++ Code/Game/maps/Mazmorra2.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mazmorra" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1085" columns="31">
+ <image source="Mazmorra.png" trans="ff55ff" width="541" height="600"/>
+ <terraintypes>
+  <terrain name="Nuevo Terreno" tile="-1"/>
+ </terraintypes>
+ <tile id="192" terrain=",,0,"/>
+</tileset>
